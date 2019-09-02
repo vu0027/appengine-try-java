cd698141168b96b9632f066a53f8d1232136ee68
Anh Vu <46767310+vu0027@users.noreply.github.com>
2019-09-01 20:57:05 -0500

    Update DemoServlet.java

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -30,18 +30,6 @@ public class DemoServlet extends HttpServlet {
       throws IOException {
     resp.setContentType("text/plain");
     resp.getWriter().println("{ \"name\": \"World\" }");
-    // create instance of Random class 
-        Random rand = new Random(); 
-        List<Integer> list = new ArrayList<Integer>();
-
-        // Generate random integers in range 0 to 999 
-        int rand_int1 = 0;
-        for(int i = 0; i < 1000; i++) {
-        // Print random integers 
-            rand_int1 = rand.nextInt(1000000000); 
-            list.add(rand_int1);
-        	System.out.println(rand_int1); 
-          out.println(rand_int1);
-        }
+    
   }
 }
