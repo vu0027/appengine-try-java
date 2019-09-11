f9651fdbc1bdc82e77df4669a8c66741cfb3c861
Anh Vu <46767310+vu0027@users.noreply.github.com>
2019-09-11 16:16:41 -0500

    Update DemoServlet.java

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -13,6 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random; 
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
 
 public class DemoServlet extends HttpServlet {
   @Override
